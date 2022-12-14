196ee9d82bd885b206d6884b201ed1ba364b5832
Asma-A-S <asma.benzerrouk@gmail.com>
2022-12-14 15:32:29 +0100

    code fonctionnel pour récupération des articles retournés par l'API

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,29 +1,33 @@
-function fetchCards(){
-    fetch(" http://localhost:3000/api/products")
-    .then(function(res){
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then((data)=>{
-        console.log(data);
-
-        let productItems = document.getElementById("items");
-
-        for (i=0; i<data.length; i++) {
-            const productItem = 
-            <a href ="./product.html?id=${data[i]._id}">
-                <article>
-                    <img 
-                        src="${data[i].imageUrl}" 
-                        alt="${data[i].altTxt}"
-                    />
-                    <h3 class="productName">${data[i].name}</h3>
-                    <p class="productDescription">${data[i].description}</p>
-                </article>
-            </a>;
-            productItems.innerHTML += productItem;
-        }
-    })
+main()
+async function main() {
+    const products = await getProducts()
+    for (product of products) {
+        displayProduct(products)
+    }
 }
-fetchCards();
+
+function getProducts() {
+    return fetch("http://localhost:3000/api/products")
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(products) {
+            return products
+        })
+        .catch(function(error) {
+            alert(error)
+        })
+}
+function displayProduct(){
+    document.getElementById("items").innerHTML += `
+        <a href ="./product.html?id=${product._id}">
+            <article>
+                <img 
+                    src="${product.imageUrl}" 
+                    alt="${product.altTxt}"
+                />
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+            </article>
+        </a>`  
+}
