7979113577b3d646f1d3bf71cd3569f00a10cca2
Asma-A-S <asma.benzerrouk@gmail.com>
2022-12-14 21:50:55 +0100

    récupération et affichage de l'image produit, son title, son price et sa description

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,32 +1,34 @@
-(async function(){
+(async function() {
     const productId = getProductId()
     const product = await getProduct(productId)
-    displayProdcut(product)
+    hydrateProduct(product)
 })()
 
-function getProductId(){
+function getProductId() {
     return new URL(location.href).searchParams.get("id")
 }
+
 function getProduct(productId) {
-    return fetch("http://localhost:3000/api/products/${productId}")
-        .then(function(response) {
+    return fetch(`http://localhost:3000/api/products/${productId}`)
+        .then (function(response){
             return response.json()
         })
-        .then(function(product) {
+        .then (function(product) {
             return product
         })
         .catch(function(error) {
-            alert(error)
+            alert(error);
         })
 }
-function displayProdcut(product) {
-    let img = document.getElementById("item__img");
-    img.innerHTML = `<img src="${products.imageUrl}" alt="${products.altTxt}">`;
-    document.getElementById("title").textContent = product.name;
-    document.getElementById("price").textContent = product.price;
-    document.getElementById("description").textContent = product.description;
-    let color = document.getElementById("colors");
+
+function hydrateProduct(product) {
+    document.getElementById("title").textContent = product.name
+    document.getElementById("price").textContent = product.price
+    document.getElementById("description").textContent = product.description
+    let image = document.querySelector(".item__img");
+    image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+    /*let color = document.getElementById("colors");
     for (i=0; i<products.color.length; i++) {
         color.innerHTML += `option value="${products.colors[i]}">${products.colors[i]}</option>`
-    }
+    }*/
 }
