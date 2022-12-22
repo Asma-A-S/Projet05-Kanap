0f6fc77f57b6464aeb34f0ba4ac51d8c72cc8cb4
Asma-A-S <asma.benzerrouk@gmail.com>
2022-12-22 02:07:48 +0100

    incrémenter quantité produit si même Id et même couleur

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,3 +1,21 @@
+/*const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const id = urlParams.get("id")
+
+fetch('http://localhost:3000/api/products/${id}')
+    .then((response) => response.json())
+    .then((res) => handleData(res))
+
+function handleData(product) {
+    const altTxt = product.altTxt
+    const colors = product.colors
+    const description = product.description
+    const imageUrl = product.imageUrl
+    const price = product.price
+    const _id = product._id
+
+}
+*/
 (async function() {
     const productId = getProductId()
     const product = await getProduct(productId)
@@ -13,16 +31,15 @@ function getProduct(productId) {
         .then (function(response){
             return response.json()
         })
-        .then (function(product) {
-            return product
+        .then (function(product){
+        return product
         })
         .catch(function(error) {
             alert(error);
         })
 }
-
 function hydrateProduct(product) {
-    document.getElementById("title").textContent = product.name
+    document.querySelector("#title").textContent = product.name
     document.getElementById("price").textContent = product.price
     document.getElementById("description").textContent = product.description
     let image = document.querySelector(".item__img");
@@ -32,18 +49,6 @@ function hydrateProduct(product) {
         color.innerHTML += `<option value="${product.colors[i]}">${product.colors[i]}</option>`
     }
 }
-
-//input pour la quantité
-/*function quantityvalue(){
-    const itemQuantity = document.getElementById("quantity").value
-    return itemQuantity
-}
-//récupérer la couleur choisi du produit
-function colorValue(){
-    const color = document.getElementById("colors").value
-    return color
-}*/
-
 //récupérer les articles dans le panier
 const button = document.querySelector("#addToCart")
 if (button != null){
@@ -52,12 +57,31 @@ if (button != null){
     const color = document.getElementById("colors").value
     if (color == null || color == "" || itemQuantity == null || itemQuantity==0){
         alert("please select color and quantity")
+        return
     }
     let cart = {
         color: color,
-        quantity: itemQuantity,
+        quantity: parseInt(itemQuantity),
+        Id: getProductId(),
     }
-    let cartlinea = JSON.stringify(cart);
-    localStorage.setItem("cart", cartlinea)
-})
-}
+    let panier = JSON.parse(localStorage.getItem("cart"));
+    console.log(panier, typeof panier)
+    if (panier == null ){
+        panier = [];
+        panier.push(cart)
+    }
+    else{
+        for (i = 0; i <panier.length; i++){
+            if(
+                panier[i].Id == cart.Id &&
+                panier[i].color == cart.color
+            ){
+                return panier[i].quantity =  panier[i].quantity + cart.quantity
+            }
+        }
+            panier.push(cart)
+        }
+    localStorage.setItem("cart", JSON.stringify(panier));
+    window.location.href = "cart.html"
+})}
+
