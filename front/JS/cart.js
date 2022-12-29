8bcbab48acd53fc85d0f8d6e2465693d0de8cc22
Asma-A-S <asma.benzerrouk@gmail.com>
2022-12-29 14:55:00 +0100

    coder fonction deleteItem pour supprimer article du panier, fonctionnelle

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -20,38 +20,131 @@ function getProduct(productId) {
   let totalQuantity = 0;
   let totalPrice = 0;
   for (product of cartPanier) {
-    const article = await getProduct(product.Id)
+    const item = await getProduct(product.Id)
     totalQuantity += product.quantity
-    totalPrice += (product.quantity * article.price)
-    console.log(article)
-    displayProduct(article)
+    totalPrice += (product.quantity * item.price)
+    console.log(item)
+    displayProduct(item)
+    deleteProduct()
   }
   document.getElementById("totalQuantity").innerHTML = totalQuantity;
   document.getElementById("totalPrice").innerHTML = totalPrice
 })()
-function displayProduct(article) {
-  document.getElementById("cart__items").innerHTML += `
-<article class="cart__item" data-id="${product.Id}" data-color="${product.color}">
-  <div class="cart__item__img">
-  <img src="${article.imageUrl}" alt="${article.altTxt}">
-</div>
-<div class="cart__item__content">
-  <div class="cart__item__content__description">
-    <h2>${article.name}</h2>
-    <p>${product.color}</p>
-    <p>${article.price} &euro;</p>
-  </div>
-  <div class="cart__item__content__settings">
-    <div class="cart__item__content__settings__quantity">
-      <p>Qté : </p>
-      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${product.quantity}>
-    </div>
-    <div class="cart__item__content__settings__delete">
-      <p class="deleteItem">Supprimer</p>
-    </div>
-  </div>
-</div>
-</article>`;
+
+/*création de la balise article*/
+function displayProduct(item){
+//création de la balise article 
+  let parent = document.getElementById("cart__items")
+  let child = document.createElement("article")
+  parent.appendChild(child)
+  child.className = "cart__item";
+  child.setAttribute("data-id", product.Id);
+  child.setAttribute("data-color", product.color);
+
+// création de la div image et insertion image
+  let divImage = document.createElement("div")
+  child.appendChild(divImage)
+  divImage.className = "cart__item__img"
+  let img = document.createElement("img")
+  divImage.appendChild(img)
+  img.setAttribute("src", item.imageUrl)
+  img.setAttribute("alt", item.altTxt)
+
+//balise content
+let divContent = document.createElement("div")
+child.appendChild(divContent)
+divContent.className = "cart__item__content"
+
+let divDescription = document.createElement("div")
+divContent.appendChild(divDescription)
+divDescription.className = "cart__item__content__description"
+
+let title = document.createElement("h2")
+  divDescription.appendChild(title)
+title.textContent = item.name
+let itemColor = document.createElement("p")
+divDescription.appendChild(itemColor)
+itemColor.textContent = product.color
+let itemPrice = document.createElement("p")
+divDescription.appendChild(itemPrice)
+itemPrice.textContent = item.price + ` €`
+
+let settings = document.createElement("div")
+divContent.appendChild(settings)
+settings.className = "cart__item__content__settings"
+let settingsQuantity = document.createElement("div")
+settings.appendChild(settingsQuantity)
+settingsQuantity.className = "cart__item__content__settings__quantity"
+let settingsQuantityP = document.createElement("p")
+settingsQuantity.appendChild(settingsQuantityP)
+settingsQuantityP.textContent = `Qté : `
+//création balise quantité
+let settingsInput = document.createElement("input")
+settingsQuantity.appendChild(settingsInput)
+settingsInput.className = "itemQuantity"
+settingsInput.setAttribute("type", "Number")
+settingsInput.setAttribute("name", "quantity")
+settingsInput.setAttribute("min", "1")
+settingsInput.setAttribute("max", "100")
+settingsInput.setAttribute("value", product.quantity)
+
+//création bouton supprimer
+let settingsDelete = document.createElement("div")
+settings.appendChild(settingsDelete)
+settingsDelete.className = "cart__item__content__settings__delete";
+let deleteProduct = document.createElement("p")
+settingsDelete.appendChild(deleteProduct);
+deleteProduct.className = "deleteItem"
+deleteProduct.textContent = "supprimer"
 }
 
-/*supprimer un article*/
+//--------suppression---
+function deleteProduct(){
+const deleteBtn = document.querySelectorAll(".deleteItem");
+console.log(deleteBtn)
+for (let k=0; k < deleteBtn.length; k++){
+  console.log(deleteBtn[k])
+  deleteBtn[k].addEventListener("click", (event) =>{
+    event.preventDefault();
+    let itemToDelete = deleteBtn[k].closest("article");
+    let deleteId = itemToDelete.getAttribute("data-id");
+    let deleteColor = itemToDelete.getAttribute("data-color");
+    cartPanier = cartPanier.filter((element) =>
+          deleteId !== element.Id && deleteColor !== element.color);
+    localStorage.setItem("cart", JSON.stringify(cartPanier));
+    console.log("article supprimé")
+    location.reload()
+      }
+)}}
+
+/*deleteBtns.forEach((deleteBtn) =>{
+  deleteBtn.addEventListener("click", (event) =>{
+    event.preventDefault();*
+    console.log(cartPanier);
+    localStorage.setItem("cart", JSON.stringify(cartPanier))
+    itemToDelete.remove()
+    location.reload()
+  })
+})*/
+  
+
+
+
+//modifier la quantité
+/*function editQuantity (){
+  document.querySelector(".itemQuantity").addEventListener("input.value", (eve) => {
+    cart.quantity = settingsInput.value
+  }
+  )
+}*/
+
+/*formulaire*/
+
+const orderButton = document.querySelector("#order")
+orderButton.addEventListener("click", (e) => submitForm(e))
+
+function submitForm(e) {
+  e.preventDefault()
+  const form = document.querySelector(".cart__order__form")
+  console.log(form.elements)
+}
