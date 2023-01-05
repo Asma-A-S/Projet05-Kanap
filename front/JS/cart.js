d1b8031418f2cc513f73e40f96fe429f7880a3ef
Asma-A-S <asma.benzerrouk@gmail.com>
2023-01-05 22:36:28 +0100

    ajout ; et suppression de lignes vides

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -5,97 +5,91 @@ if (!cartPanier) {
 function getProduct(productId) {
   return fetch(`http://localhost:3000/api/products/${productId}`)
     .then(function (response) {
-      return response.json()
+      return response.json();
     })
     .then(function (product) {
-      return product
+      return product;
     })
     .catch(function (error) {
       alert(error);
-    })
+    });
 }
 /*var cartPanierJson = JSON.parse(cartPanier);*/
 /*console.log(cartPanierJSON)*/
-(async function () {
-  for (product of cartPanier) {
-    const item = await getProduct(product.Id)
-    console.log(item)
-    displayProduct(item)
-    editProduct()
-    deleteProduct()
-    totalQuantityPrice(item)
+(async function() {
+  for(product of cartPanier) {
+    const item = await getProduct(product.Id);
+    displayProduct(item);
+    editProduct();
+    deleteProduct();
+    totalQuantityPrice(item);
   }
 })()
-
 /*création de la balise article*/
 function displayProduct(item){
-//création de la balise article 
-  let parent = document.getElementById("cart__items")
-  let child = document.createElement("article")
-  parent.appendChild(child)
+//création de la balise article
+  let parent = document.getElementById("cart__items");
+  let child = document.createElement("article");
+  parent.appendChild(child);
   child.className = "cart__item";
   child.setAttribute("data-id", product.Id);
   child.setAttribute("data-color", product.color);
 
 // création de la div image et insertion image
-  let divImage = document.createElement("div")
-  child.appendChild(divImage)
-  divImage.className = "cart__item__img"
-  let img = document.createElement("img")
-  divImage.appendChild(img)
-  img.setAttribute("src", item.imageUrl)
-  img.setAttribute("alt", item.altTxt)
-
+  let divImage = document.createElement("div");
+  child.appendChild(divImage);
+  divImage.className = "cart__item__img";
+  let img = document.createElement("img");
+  divImage.appendChild(img);
+  img.setAttribute("src", item.imageUrl);
+  img.setAttribute("alt", item.altTxt);
 //balise content
-  let divContent = document.createElement("div")
-  child.appendChild(divContent)
-  divContent.className = "cart__item__content"
-
-  let divDescription = document.createElement("div")
-  divContent.appendChild(divDescription)
-  divDescription.className = "cart__item__content__description"
-
-  let title = document.createElement("h2")
-  divDescription.appendChild(title)
-  title.textContent = item.name
-  let itemColor = document.createElement("p")
-  divDescription.appendChild(itemColor)
-  itemColor.textContent = product.color
-  let itemPrice = document.createElement("p")
-  divDescription.appendChild(itemPrice)
-  itemPrice.textContent = item.price + ` €`
-
-  let settings = document.createElement("div")
-  divContent.appendChild(settings)
-  settings.className = "cart__item__content__settings"
-  let settingsQuantity = document.createElement("div")
-  settings.appendChild(settingsQuantity)
-  settingsQuantity.className = "cart__item__content__settings__quantity"
-  let settingsQuantityP = document.createElement("p")
-  settingsQuantity.appendChild(settingsQuantityP)
-  settingsQuantityP.textContent = `Qté : `
+  let divContent = document.createElement("div");
+  child.appendChild(divContent);
+  divContent.className = "cart__item__content";
+  let divDescription = document.createElement("div");
+  divContent.appendChild(divDescription);
+  divDescription.className = "cart__item__content__description";
+  let title = document.createElement("h2");
+  divDescription.appendChild(title);
+  title.textContent = item.name;
+  let itemColor = document.createElement("p");
+  divDescription.appendChild(itemColor);
+  itemColor.textContent = product.color;
+  let itemPrice = document.createElement("p");
+  divDescription.appendChild(itemPrice);
+  itemPrice.textContent = item.price + ` €`;
+  let settings = document.createElement("div");
+  divContent.appendChild(settings);
+  settings.className = "cart__item__content__settings";
+  let settingsQuantity = document.createElement("div");
+  settings.appendChild(settingsQuantity);
+  settingsQuantity.className = "cart__item__content__settings__quantity";
+  let settingsQuantityP = document.createElement("p");
+  settingsQuantity.appendChild(settingsQuantityP);
+  settingsQuantityP.textContent = `Qté : `;
   //création balise quantité
-  let settingsInput = document.createElement("input")
-  settingsQuantity.appendChild(settingsInput)
-  settingsInput.className = "itemQuantity"
-  settingsInput.setAttribute("type", "Number")
-  settingsInput.setAttribute("name", "quantity")
-  settingsInput.setAttribute("min", "1")
-  settingsInput.setAttribute("max", "100")
-  settingsInput.setAttribute("value", product.quantity)
+  let settingsInput = document.createElement("input");
+  settingsQuantity.appendChild(settingsInput);
+  settingsInput.className = "itemQuantity";
+  settingsInput.setAttribute("type", "Number");
+  settingsInput.setAttribute("name", "quantity");
+  settingsInput.setAttribute("min", "1");
+  settingsInput.setAttribute("max", "100");
+  settingsInput.setAttribute("value", product.quantity);
 
   //création bouton supprimer
-  let settingsDelete = document.createElement("div")
-  settings.appendChild(settingsDelete)
+  let settingsDelete = document.createElement("div");
+  settings.appendChild(settingsDelete);
   settingsDelete.className = "cart__item__content__settings__delete";
-  let deleteProduct = document.createElement("p")
+  let deleteProduct = document.createElement("p");
   settingsDelete.appendChild(deleteProduct);
-  deleteProduct.className = "deleteItem"
-  deleteProduct.textContent = "supprimer"
+  deleteProduct.className = "deleteItem";
+  deleteProduct.textContent = "supprimer";
 }
 //modifier la quantité dans la page panier, 
 function editProduct() {
-  let editQuantity = document.querySelectorAll(".itemQuantity")
+  let editQuantity = document.querySelectorAll(".itemQuantity");
   for (let n = 0; n < editQuantity.length; n++) {
     let itemToChange = editQuantity[n].closest("article");
     let itemId = itemToChange.getAttribute("data-id");
