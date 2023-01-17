354e4664f049220180f28387ae8739c4861c8386
Asma-A-S <asma.benzerrouk@gmail.com>
2023-01-17 14:25:22 +0100

    ajout fonction factory pour affichage des produits d'une façon dynamique, fonctionnel

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,5 +1,6 @@
-//fonction s'auto-appelle, récupère l'Id du produit et renvoie le produit d'une façon dynamique.
-//ensuite elle exécute la fonction hydrateProduct
+/*fonction asynchrone, elle récupère d'abord l'Id du produit, 
+elle renvoie le produit d'une façon dynamique,
+ensuite elle exécute la fonction hydrateProduct (affichage des informations du produit)*/
 (async function() {
     const productId = getProductId()
     const product = await getProduct(productId)
@@ -22,7 +23,7 @@ function getProduct(productId) {
             alert(error);
         })
 }
-//la fonction prend en paramètre product (la réponse de la requête fetch) et hydrate la carte du produit 
+//la fonction prend en paramètre product (la réponse de la requête fetch) et hydrate la card du produit 
 function hydrateProduct(product) {
     document.querySelector("#title").textContent = product.name
     document.getElementById("price").textContent = product.price
@@ -34,49 +35,60 @@ function hydrateProduct(product) {
         color.innerHTML += `<option value="${product.colors[i]}">${product.colors[i]}</option>`
     }
 }
-//récupérer les articles dans le panier
+//Ajouter les articles dans le panier
 const button = document.querySelector("#addToCart")
-if (button != null) {
-    button.addEventListener("click", (e) => {
-        const itemQuantity = document.getElementById("quantity").value
-        const color = document.getElementById("colors").value
-    
-        if (color == null || color == ""){
-            alert("please select color")
-            return
-        }
-        if (itemQuantity == null || itemQuantity == 0 || itemQuantity>100){
-            alert("Veuillez choisir une quantité comprise entre 0 et 100");
-            return
-        }
+//ajout de l'evenement click au boutton: ajouter au panier
+button.addEventListener("click", (e) => {
+    const itemQuantity = document.getElementById("quantity").value
+    const color = document.getElementById("colors").value
+    //condition: choisir une couleur produit avant l'ajout au panier
+    if (color == null || color == "") {
+        alert("please select color")
+        return
+    }
+    //condition: choisir une quantité produit avant l'ajout au panier, 0<quantité<100;
+    if (itemQuantity == null || itemQuantity == 0 || itemQuantity > 100) {
+        alert("Veuillez choisir une quantité comprise entre 0 et 100");
+        return
+    }
+    //création d'un objet cart (panier), avec les propriétés ID, color, quantity
     let cart = {
-            Id: getProductId(),
-            color: color,
-            quantity: parseInt(itemQuantity),
+        Id: getProductId(),
+        color: color,
+        quantity: parseInt(itemQuantity),
+    }
+    //récupération des données du panier enregistrée dans le localStorage
+    let panier = JSON.parse(localStorage.getItem("cart"));
+    function popupConfirmation(){
+        if (window.confirm("Article ajouté, cliquer sur ok pour être redirigé vers le pnaier")){
+            window.location.href ="cart.html"
         }
-        let panier = JSON.parse(localStorage.getItem("cart"));
-        console.log(panier)
-        if(panier == null) {
-            panier = [];
-            panier.push(cart);
-            localStorage.setItem("cart", JSON.stringify(panier))
-            console.table(panier)
+    }
+    //si le panier est vide, l'article sera ajouté au panier sans condition, et enregistre le localStorage
+    if (panier == null) {
+        panier = [];
+        panier.push(cart);
+        localStorage.setItem("cart", JSON.stringify(panier))
+        popupConfirmation()
+    }
+    //si le panier n'est pas vide
+    else if (panier){
+        //vérifier si l'article qu'on souhaite ajouté existe déjâ dans le panier,on compare la couleur et l'Id des deux produits
+        const panierExist = panier.find(
+            (panierExist) =>
+                panierExist.Id === cart.Id && panierExist.color === cart.color);
+        //si l'article est déjâ dans la panier, on met à jour la quantité du produit
+        if (panierExist) {
+            let newQuantity = parseInt(panierExist.quantity) + cart.quantity;
+            panierExist.quantity = newQuantity;
+            popupConfirmation()
         }
-        else if(panier) {
-            const panierExist = panier.find(
-                (panierExist) =>
-                    panierExist.Id === cart.Id && panierExist.color === cart.color);
-            if (panierExist) {
-                let newQuantity = parseInt(panierExist.quantity) + cart.quantity;
-                panierExist.quantity = newQuantity;
-                console.table(panier)
-            }
-            else{
+        //sinon, on ajoute l'article au panier
+        else {
             panier.push(cart)
-            console.table(panier)
-            }
-            localStorage.setItem("cart", JSON.stringify(panier));
+            popupConfirmation()
         }
-        window.location.href = "cart.html"
-    })
-}
+        //on enregistre le panier mis à jour dans le localStorage
+        localStorage.setItem("cart", JSON.stringify(panier));
+    }
+})
