faf8147bf17a43974173a99cab324dc0ef9300a3
Asma-A-S <asma.benzerrouk@gmail.com>
2023-01-17 01:11:50 +0100

    essai modification de l'affichage produits

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,11 +1,13 @@
-//fonction qui s'auto-appelle, pour chaque produit de nos produits, la fonction displayProduct s'exécute
+/*fonction asynchrone, d'abord elle récupère les products avec la fonction getProducts, 
+ensuite pour chaque produit de ces produit, elle appelle la focntion displayProduct
+(afficher les informations du produit)*/
 (async function() {
     const products = await getProducts()
     for (product of products) {
-        displayProduct(products)
+        displayProduct(product)
     }
 })()
-//récupération des produits de l'API avec une requête GET et renvoie des produits
+//récupération des produits de l'API avec une requête GET et renvoie des produits (products)
 function getProducts() {
     return fetch("http://localhost:3000/api/products")
         .then(function(response) {
@@ -19,7 +21,56 @@ function getProducts() {
         })
 }
 //insertion des produits dans le DOM 
-function displayProduct(){
+//modèle
+function displayProduct(product){
+class ProductCard {
+    constructor(product) {
+        this._id = product._id
+        this._name = product.name
+        this._description = product.description
+        this._imageUrl = product.imageUrl
+        this._altTxt = product.altTxt
+    }
+    get id() {
+        return this._id
+    }
+    get name() {
+        return this._name
+    }
+    get description() {
+        return this._description
+    }
+    get imageUrl() {
+        return this._imageUrl
+    }
+    get altTxt() {
+        return this._altTxt
+    }
+}
+//template
+class ElementCard {
+    constructor(element) {
+        this._element = element
+    }
+    createElementCard(){
+        const conteneur= document.getElementById("items")
+        const elementCard = `
+        <a href ="./product.html?id=${this._element._id}">
+            <article>
+                <img 
+                    src="${this._element.imageUrl}" 
+                    alt="${this._element.altTxt}"
+                />
+                <h3 class="productName">${this._element.name}</h3>
+                <p class="productDescription">${this._element.description}</p>
+            </article>
+        </a>`
+        conteneur.innerHTML = elementCard
+        return conteneur
+    }
+}
+}
+/*function displayProduct(){
     document.getElementById("items").innerHTML += `
         <a href ="./product.html?id=${product._id}">
             <article>
@@ -31,4 +82,4 @@ function displayProduct(){
                 <p class="productDescription">${product.description}</p>
             </article>
         </a>`
-}
+}*/
